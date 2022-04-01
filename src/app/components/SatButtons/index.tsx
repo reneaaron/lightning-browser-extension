a07c57406e254fe84a5cc3d7f3ad5726db63a337
René Aaron <rene@twentyuno.net>
2022-04-01 21:39:29 +0200

    feat: remove zap emojis

--- src/app/components/SatButtons/index.tsx
+++ src/app/components/SatButtons/index.tsx
@@ -10,25 +10,25 @@ function SatButtons({ onClick }: Props) {
     <div className="flex gap-2 mt-2">
       <Button
         icon={<SatoshiV2Icon className="w-4 h-4" />}
-        label="100 ⚡"
+        label="100"
         onClick={() => onClick("100")}
         fullWidth
       />
       <Button
         icon={<SatoshiV2Icon className="w-4 h-4" />}
-        label="1K ⚡"
+        label="1K"
         onClick={() => onClick("1000")}
         fullWidth
       />
       <Button
         icon={<SatoshiV2Icon className="w-4 h-4" />}
-        label="5K ⚡"
+        label="5K"
         onClick={() => onClick("5000")}
         fullWidth
       />
       <Button
         icon={<SatoshiV2Icon className="w-4 h-4" />}
-        label="10K ⚡"
+        label="10K"
         onClick={() => onClick("10000")}
         fullWidth
       />
--- src/app/screens/Home/index.tsx
+++ src/app/screens/Home/index.tsx
@@ -120,7 +120,7 @@ function Home() {
                 setLoadingSendSats(false);
               }
             }}
-            label="⚡️ Send Satoshis ⚡️"
+            label="⚡️ Send Satoshis"
             primary
             loading={loadingSendSats}
           />
